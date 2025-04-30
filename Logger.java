65e71f6b0a3ee10e79aae7bbb25f67b6a1814485
Michał Chojna <93717130+mchojna@users.noreply.github.com>
2025-04-30 21:30:14 +0200

    Updated project structure

--- Logger.java
+++
@@ -1,9 +0,0 @@
-public class Logger {
-    public static boolean state = false;
-
-    public static void log(String text) {
-        if(state) {
-            System.out.println(text);
-        }
-    }
-}
